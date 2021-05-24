1ce5c6cb1e21d4f70a870cd190463becdc326cbb
namelim <lim49435150@gmail.com>
2021-05-24 20:39:59 +0900

    스프링 DB 접근 기술 1. 순수 JDBC

--- src/main/java/com/inflearn/memberManage/service/SpringConfig.java
+++ src/main/java/com/inflearn/memberManage/service/SpringConfig.java
@@ -1,12 +1,30 @@
 package com.inflearn.memberManage.service;
 
+import com.inflearn.memberManage.repository.JdbcMemberRepository;
 import com.inflearn.memberManage.repository.MemberRepository;
 import com.inflearn.memberManage.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+//    private DataSource dataSource;
+
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +33,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
